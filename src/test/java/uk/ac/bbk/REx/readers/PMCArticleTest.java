cd6c93cc928af7559dd9e81d4d19fe78733f4a6d
Jan Czarnecki <jczarn01@mail.bbk.ac.uk>
2013-11-15 11:33:29 +0000

    Unit test for PMCArticle.

---
+++ src/test/java/uk/ac/bbk/REx/readers/PMCArticleTest.java
@@ -0,0 +1,70 @@
+package uk.ac.bbk.REx.readers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.*;
+import java.util.Scanner;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PMCArticleTest
+{
+    @Test
+    public void getTitleTest() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException
+    {
+        PMCArticle article = new PMCArticle("24155869");
+        assertEquals("Learning to Recognize Phenotype Candidates in the Auto-Immune Literature Using SVM Re-Ranking.",
+                article.getTitle());
+    }
+
+    @Test
+    public void getAbstractTest() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException
+    {
+        PMCArticle article = new PMCArticle("24155869");
+        Scanner abstractScanner = new Scanner(new BufferedInputStream(
+                this.getClass().getResourceAsStream("/uk/ac/bbk/REx/readers/pmcArticleAbstract.txt")));
+        abstractScanner.useDelimiter("\\A");
+        String abstractString = abstractScanner.next();
+        abstractScanner.close();
+
+        assertEquals(abstractString, article.getAbstract());
+    }
+
+    @Test
+    public void getPMCIDTest() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException
+    {
+        PMCArticle article = new PMCArticle("24155869");
+        assertEquals("3796529", article.getPMCID());
+    }
+
+    @Test
+    public void getContentTest() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException
+    {
+        PMCArticle article = new PMCArticle("24155869");
+        Scanner contentScanner = new Scanner(new BufferedInputStream(
+                this.getClass().getResourceAsStream("/uk/ac/bbk/REx/readers/pmcArticleContent.txt")));
+        contentScanner.useDelimiter("\\A");
+        String contentString = contentScanner.next();
+        contentScanner.close();
+
+        assertEquals(contentString, article.getContent());
+    }
+
+    @Test
+    public void hasPMCIDTest() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException
+    {
+        PMCArticle article = new PMCArticle("24155869");
+        assertEquals(true, article.hasPMCID());
+    }
+
+    @Test
+    public void hasPMCIDNegativeTest() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException
+    {
+        PMCArticle article = new PMCArticle("23105108");
+        assertEquals(false, article.hasPMCID());
+    }
+}
