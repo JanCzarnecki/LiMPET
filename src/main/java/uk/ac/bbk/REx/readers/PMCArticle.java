d7509cfcf5285b01c34b96eedfed9112efcc583f
Jan Czarnecki <jczarn01@mail.bbk.ac.uk>
2013-11-21 10:45:56 +0000

    Cleanup imports.

--- src/main/java/uk/ac/bbk/REx/readers/PMCArticle.java
+++ src/main/java/uk/ac/bbk/REx/readers/PMCArticle.java
@@ -13,6 +13,7 @@ import javax.xml.xpath.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.logging.Logger;
